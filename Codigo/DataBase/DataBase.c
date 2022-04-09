4293bcb6869d3bc3aa14e6de55018626affcb9a5
AimJim <92112507+AimJim@users.noreply.github.com>
2022-04-09 17:07:29 +0200

    Nada realmente

--- Codigo/DataBase/DataBase.c
+++ Codigo/DataBase/DataBase.c
@@ -8,7 +8,7 @@ int createDataBase(sqlite3 *db){
     sqlite3_stmt *statement;
     
     char sql[] = "CREATE DATABASE dataBase;";
-    int result = sqlite3_prepare_v2(db, sql, -1, &statement, NULL) ;
+    int result = sqlite3_prepare_v2(db, sql, -1, &statement, 0) ;
     printf("%s\n", sql);
     sqlite3_step(statement);
    
@@ -26,26 +26,26 @@ int createTables(sqlite3 *db){
      //Crear las cuatro tablas
     sqlite3_stmt *statement2;
     char sql2[] = "CREATE TABLE CARTAS_BLANCAS(CARDID INT NOT NULL, TEXTO VARCHAR(250), PRIMARY KEY (CARDID));";
-    int result2 = sqlite3_prepare_v2(db, sql2, -1, &statement2, NULL) ;
+    int result2 = sqlite3_prepare_v2(db, sql2, -1, &statement2, 0) ;
     sqlite3_step(statement2);
     sqlite3_finalize(statement2);
 
     sqlite3_stmt *statement3;
     char sql3[] = "CREATE TABLE CARTAS_NEGRAS(CARDID INT NOT NULL, TEXTO VARCHAR(250), PRIMARY KEY (CARDID));";
-    int result3 = sqlite3_prepare_v2(db, sql3, -1, &statement3, NULL) ;
+    int result3 = sqlite3_prepare_v2(db, sql3, -1, &statement3, 0) ;
     sqlite3_step(statement3);
     sqlite3_finalize(statement3);
 
     sqlite3_stmt *statement4;
     char sql4[] = "CREATE TABLE JUGADORES(PLAYERID INT NOT NULL, NOMBRE VARCHAR(20), PRIMARY KEY (PLAYERID));";
-    int result4 = sqlite3_prepare_v2(db, sql4, -1, &statement4, NULL) ;
+    int result4 = sqlite3_prepare_v2(db, sql4, -1, &statement4, 0) ;
     sqlite3_step(statement4);
     sqlite3_finalize(statement4);
 
 /*
     sqlite3_stmt *statement5;
     char sql5[] = "CREATE TABLE CARTAS_BLANCAS(CARDID INT NOT NULL, TEXTO VARCHAR(250), PRIMARY KEY (CARDID));";
-    int result5 = sqlite3_prepare_v2(db, sql5, -1, &statement5, NULL) ;
+    int result5 = sqlite3_prepare_v2(db, sql5, -1, &statement5, 0) ;
     sqlite3_step(statement5);
     sqlite3_finalize(statement5);
 */
@@ -59,7 +59,7 @@ int insertNewWhiteCard(sqlite3 *db, char* text, char* ID){
     strcat(sql,text);
     strcat(sql,"');");
     printf("%s\n", sql);
-    int result = sqlite3_prepare_v2(db, sql, -1, &statement, NULL) ;
+    int result = sqlite3_prepare_v2(db, sql, -1, &statement, 0) ;
     sqlite3_step(statement);
     sqlite3_finalize(statement);
     if(result != SQLITE_OK){
@@ -80,7 +80,7 @@ int insertNewBlackCard(sqlite3 *db, char* text, char* ID){
     strcat(sql,text);
     strcat(sql,"');");
     printf("%s", sql);
-    int result = sqlite3_prepare_v2(db,sql,-1,&statement, NULL);
+    int result = sqlite3_prepare_v2(db,sql,-1,&statement, 0);
     sqlite3_step(statement);
     sqlite3_finalize(statement);
     if(result != SQLITE_OK){
@@ -99,7 +99,7 @@ int insertNewPlayerData(sqlite3 *db, char* text, char* ID){
     strcat(sql,text);
     strcat(sql,"');");
     printf("%s", sql);
-    int result = sqlite3_prepare_v2(db,sql,-1,&statement, NULL);
+    int result = sqlite3_prepare_v2(db,sql,-1,&statement, 0);
     sqlite3_step(statement);
     sqlite3_finalize(statement);
     if(result != SQLITE_OK){

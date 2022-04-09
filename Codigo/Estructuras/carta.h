e7dbac0c04f06c1f17969eba677f25cb6715c2a0
Xabier <79087721+xabism97@users.noreply.github.com>
2022-04-09 18:17:53 +0200

    Estructuras

---
+++ Codigo/Estructuras/carta.h
@@ -0,0 +1,15 @@
+#ifndef CARTA_H_
+#define CARTA_H_
+
+typedef struct{
+
+int negra; //bool
+char* texto;
+int id;
+
+}Carta;
+
+imprimirCarta(Carta c);
+
+
+#endif /* CARTA_H_ */
---
+++ Codigo/Estructuras/jugador.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include "carta.h"
+#include "baraja.h"
+#include "jugador.h"
+
+void crearJugador(Jugador* jug , int numero, char* nombre , Baraja baraja){
+    jug->cartasnegras = baraja;
+    jug->nombre = nombre;
+    jug->numero = numero
+
+
+}
